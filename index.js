f4e58c73c9e48264a70eb712d54d0d83b86d4479
CAM603 <cameronhawley@comcast.net>
2019-12-05 11:52:21 -0500

    added js file

---
+++ index.js
@@ -0,0 +1,11 @@
+// Assign elements
+let digitOne = document.getElementById('digitOne');
+let digitTwo = document.getElementById('digitTwo');
+let digitThree = document.getElementById('digitThree');
+let digitFour = document.getElementById('digitFour');
+
+// Assign starting values
+let digitOneNum = 0;
+let digitTwoNum = 0;
+let digitThreeNum = 0;
+let digitFourNum = 0;
